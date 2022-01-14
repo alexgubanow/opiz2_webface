69f90b21b30a4041ebc20b1da4f25c8cb5d40537
Oleksandr Hubanov <alex@0x0h.com>
2022-01-14 16:33:24 +0200

    added usermanagement settings

--- main_app/Pages/Settings.cshtml.cs
+++ main_app/Pages/Settings.cshtml.cs
@@ -13,11 +13,22 @@ namespace main_app.Pages
 {
     public class WiFiCreds
     {
-        [Required(ErrorMessage = "Required.")]
+        //[Required(ErrorMessage = "Required.")]
         [Display(Name = "SSID")]
         public string SSID { get; set; }
 
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+    public class UserCreds
+    {
+        //[Required(ErrorMessage = "Required.")]
+        [Display(Name = "Userr")]
+        public string User { get; set; }
+
+        [DataType(DataType.Password)]
+        //[Required(ErrorMessage = "Required.")]
         [Display(Name = "Password")]
         public string Password { get; set; }
     }
@@ -26,12 +37,13 @@ namespace main_app.Pages
         private readonly ILogger<IndexModel> _logger;
         public List<string> SSIDList { get; set; }
         [BindProperty]
-        public WiFiCreds WiFiCreds { get; set; }
+        public WiFiCreds WifiCreds { get; set; }
+        [BindProperty]
+        public UserCreds UserCreds { get; set; }
         public SettingsModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
             SSIDList = new List<string>();
-            WiFiCreds = new WiFiCreds();
             try
             {
                 var fileStream = new FileStream(Environment.CurrentDirectory + "/wifilist.txt", FileMode.Open);
@@ -51,96 +63,122 @@ namespace main_app.Pages
                 _logger.LogError("Exception : {ex}\nMessage:{ex.Message}", ex, ex.Message);
             }
         }
-        public async Task<IActionResult> OnGetAsync()
+        public void OnGet()
         {
-            return Page();
         }
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostWiFIConnectAsync()
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) { return Page(); }
+            try
             {
-                try
+                if (WifiCreds.SSID != null && WifiCreds.SSID.Length > 0)
                 {
-                    if (WiFiCreds.SSID != null && WiFiCreds.SSID.Length > 0)
+                    string output = "";
+                    int status = await Utils.RunProcessAsync("nmcli", "c down hotspot", _logger, ref output);
+                    if (status == 0)
                     {
-                        string output = "";
-                        int status = await Utils.RunProcessAsync("nmcli", "c down hotspot", _logger, ref output);
+                        _logger.LogTrace("turn off hotspot");
+                    }
+                    else
+                    {
+                        Message = "Error while turn off hotspot";
+                        _logger.LogError("{}", Message);
+                    }
+                    var args = "dev wifi connect " + WifiCreds.SSID;
+                    if (WifiCreds.Password != null && WifiCreds.Password.Length > 0)
+                    {
+                        args += " password " + WifiCreds.Password;
+                    }
+                    await Task.Delay(3000);
+                    output = "";
+                    status = await Utils.RunProcessAsync("nmcli", args, _logger, ref output);
+                    if (status == 0)
+                    {
+                        _logger.LogTrace("connected to {WiFiCreds.SSID}", WifiCreds.SSID);
+                    }
+                    else
+                    {
+                        Message = "Error while connecting to " + WifiCreds.SSID;
+                        _logger.LogWarning("Error while connecting to {WiFiCreds.SSID}", WifiCreds.SSID);
+                        status = await Utils.RunProcessAsync(@"nmcli", "-t -f SSID dev wifi", _logger, ref output);
+                        System.IO.File.WriteAllText(Environment.CurrentDirectory + "/wifilist.txt", output);
+                        await Task.Delay(1000);
+                        status = await Utils.RunProcessAsync("nmcli", "c up hotspot", _logger, ref output);
                         if (status == 0)
                         {
-                            _logger.LogTrace("turn off hotspot");
+                            _logger.LogTrace("turn on hotspot");
                         }
                         else
                         {
-                            Message = "Error while turn off hotspot";
+                            Message = "Error while turn on hotspot";
                             _logger.LogError("{}", Message);
                         }
-                        var args = "dev wifi connect " + WiFiCreds.SSID;
-                        if (WiFiCreds.Password != null && WiFiCreds.Password.Length > 0)
-                        {
-                            args += " password " + WiFiCreds.Password;
-                        }
-                        await Task.Delay(3000);
-                        output = "";
-                        status = await Utils.RunProcessAsync("nmcli", args, _logger, ref output);
-                        if (status == 0)
-                        {
-                            _logger.LogTrace("connected to {WiFiCreds.SSID}", WiFiCreds.SSID);
-                        }
-                        else
-                        {
-                            Message = "Error while connecting to " + WiFiCreds.SSID;
-                            _logger.LogWarning("Error while connecting to {WiFiCreds.SSID}", WiFiCreds.SSID);
-                            status = await Utils.RunProcessAsync(@"nmcli", "-t -f SSID dev wifi", _logger, ref output);
-                            System.IO.File.WriteAllText(Environment.CurrentDirectory + "/wifilist.txt", output);
-                            await Task.Delay(1000);
-                            status = await Utils.RunProcessAsync("nmcli", "c up hotspot", _logger, ref output);
-                            if (status == 0)
-                            {
-                                _logger.LogTrace("turn on hotspot");
-                            }
-                            else
-                            {
-                                Message = "Error while turn on hotspot";
-                                _logger.LogError("{}", Message);
-                            }
-                        }
                     }
-                    //await UpdateSSIDList();
                 }
-                catch (Exception ex)
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception : {ex}\nMessage:{ex.Message}", ex, ex.Message);
+                throw;
+            }
+
+            return Page();
+        }
+        public async Task<IActionResult> OnPostUserManagementSetRootPSWDAsync()
+        {
+            if (!ModelState.IsValid) { return Page(); }
+            try
+            {
+                if (UserCreds.Password != null && UserCreds.Password.Length > 0)
                 {
-                    _logger.LogError("Exception : {ex}\nMessage:{ex.Message}", ex, ex.Message);
-                    throw;
+                    string output = "";
+                    int status = await Utils.RunShellAsync("/opt", "changepasswd.sh", "root " + UserCreds.Password, _logger, ref output);
+                    if (status == 0)
+                    {
+                        _logger.LogTrace("set new root password");
+                        Message = "new root password is set";
+                    }
+                    else
+                    {
+                        Message = "Error while setting new root password";
+                        _logger.LogError("{}", Message);
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception : {ex}\nMessage:{ex.Message}", ex, ex.Message);
+                throw;
             }
             return Page();
         }
-        //private async Task<int> UpdateSSIDList()
-        //{
-        //    string output = "";
-        //    int status = -1;
-        //    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        //    {
-        //        status = await RunProcessAsync(@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe", "netsh wlan show network  mode=bssid", ref output);
-        //        var matchList = Regex.Matches(output, @"(?<=^SSID...\: ).*(?=\s)", RegexOptions.Multiline);
-        //        SSIDList = matchList.Cast<Match>().Select(match => match.Value).ToList();
-        //    }
-        //    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        //    {
-        //        status = await RunProcessAsync(@"nmcli", "-f SSID dev wifi", ref output);
-        //        _logger.LogError("nmcli out: [{output}]", output);
-        //        if (output.Length > 0)
-        //        {
-        //            SSIDList.AddRange(output.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Skip(1));
-        //            SSIDList.RemoveAll(ssid => ssid == "--");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        SSIDList.Add("unknown arch");
-        //        return -1;
-        //    }
-        //    return status;
-        //}
+        public async Task<IActionResult> OnPostUserManagementCreateUserAsync()
+        {
+            if (!ModelState.IsValid) { return Page(); }
+            try
+            {
+                if (UserCreds.User != null && UserCreds.User.Length > 0 && UserCreds.Password != null && UserCreds.Password.Length > 0)
+                {
+                    string output = "";
+                    int status = await Utils.RunShellAsync("/opt", "addsudouser.sh", UserCreds.User + " " + UserCreds.Password, _logger, ref output);
+                    if (status == 0)
+                    {
+                        _logger.LogTrace("added new user {UserCreds.User}", UserCreds.User);
+                        Message = "added new user";
+                    }
+                    else
+                    {
+                        Message = "Error while adding new user";
+                        _logger.LogError("{}", Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception : {ex}\nMessage:{ex.Message}", ex, ex.Message);
+                throw;
+            }
+            return Page();
+        }
     }
 }
--- main_app/Utils.cs
+++ main_app/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using main_app.Pages;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,9 @@ namespace main_app
 {
     public class Utils
     {
-        public static Task<int> RunProcessAsync(string fileName, string args, ILogger<IndexModel> logger, ref string output)
+        public static Task<int> RunProcessAsync(string dir, string fileName, string args, ILogger<IndexModel> logger, ref string output)
         {
+            output = "";
             var tcs = new TaskCompletionSource<int>();
             var process = new Process
             {
@@ -18,32 +20,43 @@ namespace main_app
                     WindowStyle = ProcessWindowStyle.Hidden,
                     FileName = fileName,
                     Arguments = args,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false
+                    WorkingDirectory = dir,
+                    UseShellExecute = dir != "",
+                    RedirectStandardOutput = dir == "",
+                    RedirectStandardError = dir == ""
                 },
                 EnableRaisingEvents = true
             };
             process.Start();
-            string error = process.StandardError.ReadToEnd();
-            //output = process.StandardOutput.ReadToEnd();
-            //process.WaitForExit();
-
+            if (process.StartInfo.RedirectStandardError)
+            {
+                string error = process.StandardError.ReadToEnd();
+                if (error != null && error.Length > 0)
+                {
+                    logger.LogError("Started task [{fileName} {args}] rised error\n{output}", fileName, args, error);
+                }
+            }
             var tmpString = new StringBuilder();
             while (!process.HasExited)
             {
-                tmpString.Append(process.StandardOutput.ReadToEnd());
-            }
-            output = tmpString.ToString();
-
-            if (error != null && error.Length > 0)
-            {
-                logger.LogError("Started task [{fileName} {args}] rised error\n{output}", fileName, args, error);
+                if (process.StartInfo.RedirectStandardOutput)
+                {
+                    tmpString.Append(process.StandardOutput.ReadToEnd());
+                    output = tmpString.ToString();
+                }
             }
             logger.LogTrace("Started task [{fileName} {args}]\nOutput [{output}]", fileName, args, output);
             tcs.SetResult(process.ExitCode);
             process.Dispose();
             return tcs.Task;
+        }
+        public static Task<int> RunShellAsync(string dir, string fileName, string args, ILogger<IndexModel> logger, ref string output)
+        {
+            return RunProcessAsync(dir, fileName, args, logger, ref output);
+        }
+        public static Task<int> RunProcessAsync(string fileName, string args, ILogger<IndexModel> logger, ref string output)
+        {
+            return RunProcessAsync("", fileName, args,logger, ref  output);
         }
     }
 }
